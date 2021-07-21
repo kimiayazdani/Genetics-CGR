ff1a4712d1203dd0da99ab04e9a362416858205d
Kim <yazdanikimia@yahoo.com>
2021-07-21 18:27:44 +0430

    kspectra on case and control

--- cgr.py
+++ cgr.py
@@ -6,23 +6,28 @@ def load_sequences(filename, path="./"):
 	return np.load(path+filename)
 	
 
-def kmer_count(k_spectra, sequence, k=5):
-
+def kmer_count(sequence, k=5):
+	k_spectra = collections.defaultdict(int)
 	for i in range(len(sequence) - k + 1):
 		k_spectra[sequence[i:i+k]] += 1
 
-	print(k_spectra)
+	return(k_spectra)
+
 
 
 if __name__ == "__main__":
 	case = load_sequences(filename="case.npy")
 	control = load_sequences(filename="control.npy")
 	k = 5
-	k_spectra = collections.defaultdict(int)
+	# k_spectra = collections.defaultdict(int)
 
 	print(case)
 	print(control)
 	
-	
-	kmer_count(sequence=case[4], k_spectra=k_spectra)
-	
+
+	case_kspectra = list(map(kmer_count, case))
+	control_kspectra = list(map(kmer_count, control))
+
+	print(case_kspectra)
+
+	# what should I think about... how can do it on one efficiently (the back and forth method)
